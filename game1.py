9c0cc4428bc6f803ef0afbb754ec6ae63a81894a
Shikhar Srivastava <sshikhar494@gmail.com>
2024-03-19 12:50:38 +0530

    .

--- game1.py
+++ game1.py
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
 from turtle import Screen
-=======
-# pip install pgzero
->>>>>>> d5e9b39cb9910f3de65af2522a3b16be51dfd2cf
 import pgzrun
 
 WIDTH = 800
 HEIGHT = 500
 
 def draw():
-<<<<<<< HEAD
     Screen.draw.text(
-=======
-    screen.draw.text(
->>>>>>> d5e9b39cb9910f3de65af2522a3b16be51dfd2cf
         "Hello, Pygame Zero!",
         center=(WIDTH/2, HEIGHT/2),
         fontsize=32,
--- questions.py
+++ questions.py
@@ -5,8 +5,4 @@ print(rev_value, value)
 if str(value) == rev_value:
     print("The number is a pallindrom")
 else:
-<<<<<<< HEAD
     print("The number is not a pallindrom")
-=======
-    print("The number is not a pallindrom")
->>>>>>> d5e9b39cb9910f3de65af2522a3b16be51dfd2cf
